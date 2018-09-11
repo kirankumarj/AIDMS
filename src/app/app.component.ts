b3a6e2ca7817a6f204ff1caa9ac352b3acfccba0
Kiran Kumar J <itsoftkiran@gmail.com>
2018-09-11 14:44:44 +0530

    Highlight background color of selected menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  linkActive = 'dashboard';
+  presentValue;
+  originalPath: string;
+  constructor(private locaton: Location) {
+  }
+  ngOnInit() {
+    this.originalPath = window.location.pathname;
+    if (this.originalPath.substr(1) === 'dashboard' || this.originalPath.substr(1) === '') {
+      this.linkActive = 'dashboard';
+     } else {
+      this.linkActive = this.originalPath.substr(1, this.originalPath.indexOf('-') - 1);
+    }
+    console.log(this.linkActive);
+  }
+  activeClass(linkactive: string) {
+        this.linkActive = linkactive;
+  }
 }
